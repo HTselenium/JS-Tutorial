34634899760010794cdb2835f73b66e16f9c4b45
Hasbur bin Hishamuddin <hishamhr@basfad.basf.net>
2023-12-18 15:34:47 +0800

    Feat: Refactor the AI Model instructions.

--- main.py
+++ main.py
@@ -13,55 +13,65 @@ openai.api_type = os.getenv("OPENAI_API_TYPE")
 openai.api_version = os.getenv("OPENAI_API_VERSION")
 openai.api_base = os.getenv("OPENAI_API_BASE")
 
-headers = {
-    'Authorization': f'token {token}',
-    'Accept': 'application/vnd.github+json'
-}
+headers = {"Authorization": f"token {token}", "Accept": "application/vnd.github+json"}
 
-url = f'https://api.github.com/repos/{user}/{repo}/pulls'
+url = f"https://api.github.com/repos/{user}/{repo}/pulls"
 response = requests.get(url, headers=headers)
 
 if response.status_code == 200:
     data = response.json()
     if data:
-        last_processed_pr = max(pull_request['number'] for pull_request in data)
+        last_processed_pr = max(pull_request["number"] for pull_request in data)
         print(f"Last Processed Pull Request Number: {last_processed_pr}")
-        
+
         # Fetching diff for the first pull request
-        if 'diff_url' in data[0]:
-            diff_url = data[0]['diff_url']
+        if "diff_url" in data[0]:
+            diff_url = data[0]["diff_url"]
             diff_response = requests.get(diff_url, headers=headers)
             if diff_response.status_code == 200:
-                diff = diff_response.text 
+                # Fetching the last line of the diff
+                diff = diff_response.text
 
                 messages = [
-                        {"role": "system", "content": "You are github PR reviwer assistant. "},
-                        {"role": "user", "content": f"Analyze this pull request text and provide a review and check if there is an error in the code:\n\n{diff}"}
-                    ]
+                    {
+                        "role": "system",
+                        "content": """You are a world-class helpful assistant. You are a superintelligent AI \
+                        with one mission: to provide as much descriptive as possible answers to \
+                        the users during a Pull Request Code Review. \
+                        Provide a detailed analysis on the changes made in a well-formatted form. \
+                        If any syntax or logic errors in the code, please provide recommendation to improve the code. \
+                        Always answer in English.""",
+                    },
+                    {
+                        "role": "user",
+                        "content": f"Please review the following pull request : {diff}",
+                    },
+                ]
 
                 response = openai.ChatCompletion.create(
-                        engine="gpt-4",
-                        messages=messages,
-                    )
+                    engine="gpt-4",
+                    messages=messages,
+                )
 
-                review = response.choices[0].message['content'].strip()
+                review = response.choices[0].message["content"].strip()
                 review = f"Review from GPT \n\n{review}"
 
                 print(diff)
 
-                comment_url = f'https://api.github.com/repos/{user}/{repo}/issues/{last_processed_pr}/comments'
+                comment_url = f"https://api.github.com/repos/{user}/{repo}/issues/{last_processed_pr}/comments"
 
-                comment_data = {
-                    'body': review,
-                    'user': "PR-Reviewer-Bot"
-                }
+                comment_data = {"body": review, "user": "PR-Reviewer-Bot"}
 
-                comment_response = requests.post(comment_url, json=comment_data, headers=headers)
+                comment_response = requests.post(
+                    comment_url, json=comment_data, headers=headers
+                )
 
                 if comment_response.status_code == 201:
                     print("Review successfully posted on GitHub.")
                 else:
-                    print(f"Failed to post review. Status code: {comment_response.status_code}")
+                    print(
+                        f"Failed to post review. Status code: {comment_response.status_code}"
+                    )
                     print(f"Response: {comment_response.json()}")
             else:
                 print(f"Failed to fetch diff for PR: {diff_response.status_code}")
